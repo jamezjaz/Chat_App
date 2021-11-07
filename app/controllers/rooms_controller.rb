84337ac8eab10ab58653e5cb6fd5099bfd66b41c
Jamezjaz <jamezjaz@gmail.com>
2021-11-07 20:07:31 +0100

    feat: Add bootstrap classes that divides the page in order to accommodate the chat box

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,13 @@
 class RoomsController < ApplicationController
   def index
+    @room = Room.new
     @current_user = current_user
     redirect_to '/signin' unless @current_user
     @rooms = Room.public_rooms
     @users = User.all_except(@current_user)
+  end
+
+  def create
+    @room = Room.create(name: params["room"]["name"])
   end
 end
