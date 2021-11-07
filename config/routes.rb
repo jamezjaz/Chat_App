408b9dea79607ad70c9e63c3de9e19e18670b031
Jamezjaz <jamezjaz@gmail.com>
2021-11-07 17:21:22 +0100

    feat: Displays user's username
    feat: Render all rooms and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'rooms/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/signin', to: 'session#new'
-  post '/signin', to: 'session#create'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
 
   resources :rooms
